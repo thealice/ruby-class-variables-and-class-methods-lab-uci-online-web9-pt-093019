2d307583d2031f6370ad34d0792ca66f883bd48a
Alice Freda <alicefreda@gmail.com>
2019-10-24 01:09:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -26,11 +26,13 @@ class Song
   def self.genre_count
       genre_count = {}
       @@genres.each do |genre|
-        if genre_count[genre]
-          genre_count[genre] += 1
-        else
-          genre_count[genre] = 1
-        end
+        genre_count[genre] ||= 0
+        genre_count[genre] += 1
+        # if genre_count[genre]
+        #   genre_count[genre] += 1
+        # else
+        #   genre_count[genre] = 1
+        # end
       end
       genre_count
     end
